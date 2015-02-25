5a9da44e8690a4a642fd96cf977c939951f61a8f
Hibiki08 <devilcatt1@gmail.com>
2015-02-25 14:51:00 +0300

    Добавлена автозагрузка

---
+++ autoload.php
@@ -0,0 +1,10 @@
+<?php
+
+function __autoload($class) {
+    if (file_exists(__DIR__ . '/classes/' . $class . '.php')) {
+       require_once __DIR__ . '/classes/' . $class . '.php';
+    }
+    elseif (file_exists(__DIR__ . '/models/' . $class . '.php')) {
+        require_once __DIR__ . '/models' . $class . '.php';
+    }
+}
