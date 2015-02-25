fa8c3512c633a3bb123b2f207518166879cc47a7
Hibiki08 <devilcatt1@gmail.com>
2015-02-25 15:12:45 +0300

    Исправлены адреса

--- autoload.php
+++ autoload.php
@@ -5,6 +5,9 @@ function __autoload($class) {
        require_once __DIR__ . '/classes/' . $class . '.php';
     }
     elseif (file_exists(__DIR__ . '/models/' . $class . '.php')) {
-        require_once __DIR__ . '/models' . $class . '.php';
+        require_once __DIR__ . '/models/' . $class . '.php';
+    }
+    elseif (file_exists(__DIR__ . '/controllers/' . $class . '.php')) {
+        require_once __DIR__ . '/controllers/' . $class . '.php';
     }
 }
