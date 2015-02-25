76336667b397c1424b46c7451389ba637eda50aa
Hibiki08 <devilcatt1@gmail.com>
2015-02-25 16:50:10 +0300

    Разметка одной новости

---
+++ views/one.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Все новости</title>
+</head>
+<body>
+
+    <h1><?php echo $object->title; ?></h1>
+    <p><?php echo $object->text; ?></p>
+
+</body>
+</html>
