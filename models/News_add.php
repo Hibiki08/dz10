98c33041e32048dba603116120610d4560945c85
Hibiki08 <devilcatt1@gmail.com>
2015-02-26 13:22:15 +0300

    Добавлен класс с методом добавления новости

---
+++ models/News_add.php
@@ -0,0 +1,12 @@
+<?php
+
+class News_add {
+    public static function add() {
+
+        $res = new SQL;
+        $sql = "INSERT INTO newss (title, text) VALUES ('" . $_POST['title'] . "'" . "," .
+            "'" . $_POST['text'] . "')";
+        $res->query($sql);
+    }
+
+} 
