0b81b16738942d5a2609d3de3abdfadb099f28b5
Hibiki08 <devilcatt1@gmail.com>
2015-02-26 12:45:19 +0300

    Добавлена форма

---
+++ views/form.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Все новости</title>
+    </head>
+    <body>
+        <form action="/../" method="post">
+            <label>Название:<br/>
+            <input type="text" name="title">
+            </label><br/>
+            <label>Текст:<br/>
+            <textarea name="text"></textarea><br/>
+            </label>
+            <input type="submit"/>
+        </form>
+    </body>
+</html>
