0f52fc742df34e43dcba8cd6f56971cd63a287fd
Hibiki08 <devilcatt1@gmail.com>
2015-02-26 12:47:30 +0300

    Изменены условия Гет, перенесена строчка с нововстями

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php
 require_once __DIR__ . '/autoload.php';
 
-NewsController::actionAll();
-
 if (isset($_GET['cntr'])) {
 
     $news = $_GET['cntr'] . 'Controller';
-
-    if (file_exists(__DIR__ . '/controllers/' . $news . '.php')) {
-
-        if (isset($_GET['act'])) {
-
-            $act = 'action' . $_GET['act'];
-            $news::$act();
-        }
-    }
 }
+
+elseif (isset($_GET['act'])) {
+
+    $act = 'action' . $_GET['act'];
+    $news::$act();
+}
+
+NewsController::actionAll();
+
+
 
 
