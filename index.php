8f3076a6a17b072047fd04d34ce6ce12dff31ff0
Hibiki08 <devilcatt1@gmail.com>
2015-03-05 15:17:37 +0300

    Поменялись местами две строчки

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ $ctrl = isset($_GET['ctrl']) ? $_GET['ctrl'] : 'News';
 $act = isset($_GET['act']) ? $_GET['act'] : 'All';
 
 $controllerName = $ctrl . 'Controller';
-$action = 'action' . $act;
 
 $controller = new $controllerName;
+
+$action = 'action' . $act;
 $controller->$action();
