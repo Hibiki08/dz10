3c585263221f214a4c6a378de422d4de0ca2606e
Hibiki08 <devilcatt1@gmail.com>
2015-03-05 15:22:04 +0300

    Добавлен поисковик

---
+++ views/search.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Все новости</title>
+</head>
+<body>
+<form action="index.php?act=Find" method="post">
+        <input type="search" name="searchTitle" value="Искать по названию" />
+    <input type="submit" value="Искать"/>
+</form>
+
+    <form action="index.php?act=Find" method="post">
+        <input type="search" name="searchText" value="Искать по содержимому" />
+    <input type="submit" value="Искать"/>
+</form>
+</body>
+</html>
