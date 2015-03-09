61e364d1c01ebf546985e85abf311ece35bd3a76
Hibiki08 <devilcatt1@gmail.com>
2015-03-09 16:20:24 +0300

    Ловим исключение

--- index.php
+++ index.php
@@ -6,7 +6,24 @@ $act = isset($_GET['act']) ? $_GET['act'] : 'All';
 
 $controllerName = $ctrl . 'Controller';
 
-$controller = new $controllerName;
+try {
+    $controller = new $controllerName;
 
-$action = 'action' . $act;
-$controller->$action();
+    $action = 'action' . $act;
+    $controller->$action();
+}
+catch (E404Exception $e) {
+
+    $date = date('m-d-Y/H-i-s');
+    $file = $e->getFile();
+    $line = $e->getLine();
+    $message = $e->getMessage();
+
+    $log = new LogEcxeption();
+    $log->dataLog($date, $file, $line, $message);
+
+    header("HTTP/1.0 404 Not Found");
+    $view = new View();
+    $view->error = $e->getMessage();
+    $view->display('404.php');
+}
