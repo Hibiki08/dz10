528f0b98a57023832b5b5dd27a35b0b8ee4f77ea
Hibiki08 <devilcatt1@gmail.com>
2015-03-09 16:22:59 +0300

    Бросаем исключение в случае ошибки

--- classes/SQL.php
+++ classes/SQL.php
@@ -7,7 +7,27 @@ class SQL {
 
     public function __construct()
     {
-       $this->dbh = new PDO('mysql:dbname=ШП;host=localhost', 'root', '');
+        try {
+            $this->dbh = new PDO('mysql:dbname=ШП;host=localhost', 'root', '');
+            $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e) {
+            header('HTTP/1.0 403 Forbidden');
+            $e = new PDOException('Что-то пошло не так');
+
+            $date = date('m-d-Y/H-i-s');
+            $file = $e->getFile();
+            $line = $e->getLine();
+            $message = $e->getMessage();
+
+            $log = new LogEcxeption;
+            $log->dataLog($date, $file, $line, $message);
+
+            $view = new View();
+            $view->error = $e->getMessage();
+            $view->display('403.php');
+            die;
+        }
     }
 
     public function className($className) {
@@ -17,9 +37,9 @@ class SQL {
 
     public function query($sql, $parameters=[]) {
 
-        $res = $this->dbh->prepare($sql);
-        $res->execute($parameters);
-        return $res->fetchAll(PDO::FETCH_CLASS, $this->class);
+            $res = $this->dbh->prepare($sql);
+            $res->execute($parameters);
+            return $res->fetchAll(PDO::FETCH_CLASS, $this->class);
     }
 
     public function queryOther($sql, $parameters=[]) {
